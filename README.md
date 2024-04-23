2199e11f158f4aa50afd1cb5691021e5ad045f27
Pierre-Louis Létoquart <pierrelouislet@gmail.com>
2024-04-23 14:58:03 -0400

    minor updates

--- README.md
+++ README.md
@@ -6,6 +6,29 @@ The next generation search engine.
 
 This project is a simple POC to demonstrate the use of RAG, PS, and simple summurization techniques to create a search engine that can generate THE answer to a query.
 
+## Google Custom Search JSON API
+
+For the step of fetching web resources. Requires a Google account.
+
+### Query
+
+Three parameters are required for each request to the Rest API:
+
+- `key` : the Google API key ([find here](https://console.cloud.google.com/apis))
+- `cx` : the search engine ID ([find here](https://programmablesearchengine.google.com/controlpanel/all))
+- `q` : the query text itself
+
+Other optional parameters can be used, we will use the following ones:
+
+- `lr`: the language of content in response
+- `filter`: avoid duplicate content in response
+
+> Note: the request body must be empty
+
+### Results
+
+The response type is described [here](https://developers.google.com/custom-search/v1/reference/rest/v1/Search).
+
 ## To explore
 
 To summurize content:
--- src/app/api/results/route.ts
+++ src/app/api/results/route.ts
@@ -1,22 +1,4 @@
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OllamaEmbeddings } from "@langchain/community/embeddings/ollama";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { createRetrievalChain } from "langchain/chains/retrieval";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-
-const ollama = new ChatOllama({
-  baseUrl: "http://localhost:11434",
-  model: "llama3",
-});
-
-const embeddings = new OllamaEmbeddings({
-  model: "nomic-embed-text",
-  maxConcurrency: 5,
-});
 
 export async function POST(req: Request) {
   const { query } = await req.json();
@@ -47,55 +29,12 @@ export async function POST(req: Request) {
       : item.pagemap.cse_thumbnail[0].src || "",
   }));
 
-  console.log(items);
-
   const resources = await Promise.all(
     items.map(async (item: any) => {
       const loader = new CheerioWebBaseLoader(item.link);
-      const docs = await loader.load();
-
-      const splitter = new RecursiveCharacterTextSplitter();
-      const splitDocs = await splitter.splitDocuments(docs);
-
-      const outputParser = new StringOutputParser();
-
-      const vectorstore = await MemoryVectorStore.fromDocuments(
-        splitDocs,
-        embeddings,
-      );
-
-      const prompt = ChatPromptTemplate.fromTemplate(`
-          As a search pro, use the web to find info for every user question. Use all the search results to give more info and help in your response. If there are related images, add them too. Answer the user's question directly and make your response better with info from the search results. Always give the source URL when using info from a specific website. Make sure to use the same language as the user in your response.
-
-          !!Answer the following question based only on the provided context:
-
-      <context>
-      {context}
-      </context>
-
-      Question: {input}`);
-
-      const documentChain = (
-        await createStuffDocumentsChain({
-          llm: ollama,
-          prompt,
-        })
-      ).pipe(outputParser);
-
-      const retriever = vectorstore.asRetriever();
-
-      const retrievalChain = await createRetrievalChain({
-        combineDocsChain: documentChain,
-        retriever,
-      });
-
-      return await retrievalChain.invoke({
-        input: query,
-      });
+      return await loader.load();
     }),
   );
-
-  console.log(resources);
 
   return new Response(
     JSON.stringify(
